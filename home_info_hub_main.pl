36eb15fd4995f4492efc5cf58ed8d1906efe439a
M. Moccaro <mfm2772@comcast.net>
2017-02-21 20:45:18 -0500

    Create home_info_hub_main.pl

---
+++ home_info_hub_main.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+#  File: home_info_hub_main.pl
+#  Type: Perl Source File 
+#  Date: February 21, 2017 
+# 
+#  Description: 
+#  This is the main file for the Home Info Hub project.
+
+#Pragmas
+use Gtk2;
+
+Gtk2->init;
+
+$window = Gtk2::Window->new('toplevel');
+
+$window->signal_connect (destroy => sub { Gtk2->main_quit; });
+
+$window->show_all;
+
+Gtk2->main;
+
+exit(0);
