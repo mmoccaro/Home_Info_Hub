56427a675f25bc2631d107e3ac351d126aa477c6
M. Moccaro <mfm2772@comcast.net>
2017-03-10 14:07:16 -0600

    Update home_info_hub_main.pl

--- home_info_hub_main.pl
+++ home_info_hub_main.pl
@@ -1,11 +1,11 @@
-my  #!/usr/bin/perl 
+#!/usr/bin/env perl 
  
- #  File: home_info_hub_main.pl 
- #  Type: Perl Source File  
- #  Date: February 21, 2017  
- #  
- #  Description:  
- #  This is the main file for the Home Info Hub project. 
+#  File: home_info_hub_main.pl 
+#  Type: Perl Source File  
+#  Date: February 21, 2017  
+#  
+#  Description:  
+#  This is the main file for the Home Info Hub project. 
  
 #Pragmas
 use strict;
@@ -13,14 +13,16 @@ use warnings;
 use utf8;
 use Gtk2; 
  
-Gtk2->init; 
- 
-my $window = Gtk2::Window->new('toplevel'); 
- 
+#Initialize GTK and create a main window
+Gtk2->init;  
+my $window = Gtk2::Window->new('toplevel');  
 $window->signal_connect (destroy => sub { Gtk2->main_quit; }); 
- 
+
+#Show the window and its components
 $window->show_all; 
- 
+
+#Start The Main Loop
 Gtk2->main; 
 
+#Exit Success
 exit(0);
