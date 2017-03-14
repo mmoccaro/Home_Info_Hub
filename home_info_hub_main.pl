bd920675a1d503e778890aeacbf9e735d4908049
M. Moccaro <mfm2772@comcast.net>
2017-03-13 21:29:25 -0500

    Update home_info_hub_main.pl

--- home_info_hub_main.pl
+++ home_info_hub_main.pl
@@ -20,6 +20,21 @@ $window->set_decorated(0);
 $window->set_position('center');
 $window->maximize();
 
+#Button
+my $wifi_button = Gtk2::Button->new('WiFi Password');
+my $temp_button = Gtk2::Button->new('Temperature');
+my $internet_button = Gtk2::Button->new('Internet Status');
+my $phone_button = Gtk2::Button->new('Phone Numbers');
+
+#Create A Table
+my $table = Gtk2::Table->new(4,2, 'FALSE');
+$table->attach_defaults($wifi_button, 1, 2, 0, 1);
+$table->attach_defaults($temp_button, 1, 2, 1, 2);
+$table->attach_defaults($internet_button, 1, 2, 2, 3);
+$table->attach_defaults($phone_button, 1, 2, 3, 4);
+
+$window->add($table);
+
 #Show the window and its components
 $window->show_all; 
 
