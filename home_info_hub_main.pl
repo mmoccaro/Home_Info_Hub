0044f6bf363a36d8c18dc0c4afbe049ae95f335a
M. Moccaro <mfm2772@comcast.net>
2017-03-13 20:16:18 -0500

    Update home_info_hub_main.pl

--- home_info_hub_main.pl
+++ home_info_hub_main.pl
@@ -11,12 +11,14 @@
 use strict;
 use warnings;
 use utf8;
-use Gtk2; 
+use Gtk2 -init; 
  
-#Initialize GTK and create a main window
-Gtk2->init;  
+#Initialize GTK and create a main window 
 my $window = Gtk2::Window->new('toplevel');  
 $window->signal_connect (destroy => sub { Gtk2->main_quit; }); 
+$window->set_decorated(0);
+$window->set_position('center');
+$window->maximize();
 
 #Show the window and its components
 $window->show_all; 
